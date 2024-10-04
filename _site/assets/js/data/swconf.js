787d27c1f6823a263867204ce9d69f0e1bb0fa18
Minhtuannn <ngominhtuan311@gmail.com>
2024-10-04 17:59:08 +0000

    hide _site

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728063534',resources: [
+    cacheName: 'chirpy-1728064282',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
