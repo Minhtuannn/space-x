7d23b00a4f42d20289478b8f2cc55e4a9a8d8562
Minhtuannn <ngominhtuan311@gmail.com>
2024-10-04 17:30:47 +0000

    update config vs post

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728060542',resources: [
+    cacheName: 'chirpy-1728062916',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
